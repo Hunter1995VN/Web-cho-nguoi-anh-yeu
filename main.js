ac5dacad511017dfe4492aed1b0e4cbce09c9f6a
Hunter1995VN <nhiemvothanh2004@gmail.com>
2024-02-29 12:49:41 +0700

    Yêu em <3

---
+++ main.js
@@ -0,0 +1,46 @@
+// Thay đổi nội dung búc thư ở đây
+var letterContent = "Tiên ơi, Valentine năm nào bọn mình cũng chỉ nhìn người ta phát cơm chóa hết T-T , vậy thì từ Valentine này về sau cậu làm  Valentine mỗi ngày của tớ nhé ❤. Tớ thích cậu, cậu làm người yêu mình nha UwU❤❤❤"
+
+// Tốc độ viết chữ. Số càng nhỏ tốc độ càng nhanh. 50 là tốc độ khá phù hợp
+durationWrite = 50 
+
+// Hiệu ứng gõ chữ
+
+function effectWrite () {
+    var boxLetter = document.querySelector(".letterContent")
+    letterContentSplited = letterContent.split("")
+    
+    letterContentSplited.forEach((val, index) => {
+        setTimeout(() => {
+            boxLetter.innerHTML += val    
+        }, durationWrite* index)
+    })
+}
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.querySelector(".container").classList.add("active")
+    }, 500)
+})
+
+var openBtn = document.querySelector(".openBtn")
+openBtn.addEventListener("click", () => {
+    document.querySelector(".cardValentine").classList.add("active")
+    document.querySelector(".container").classList.add("close")
+})
+
+var cardValentine = document.querySelector(".cardValentine")
+
+cardValentine.addEventListener("click", () => {
+    cardValentine.classList.toggle("open")
+
+    if(cardValentine.className.indexOf("open") != -1) {
+        setTimeout(effectWrite, 500)
+    } else {
+        setTimeout(() => {
+            document.querySelector(".letterContent").innerHTML = ""
+        }, 1000)
+    }
+})
+
+
